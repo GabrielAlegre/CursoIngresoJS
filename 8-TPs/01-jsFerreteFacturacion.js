29d47a4b73eeb4ba311599f4df7b7ecb8c08f101
GabrielAlegre <gabrielalegre68@gmail.com>
2021-07-03 22:01:10 -0300

    Nuevo Cambio

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,41 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+  let precio1, precio, precio3, suma;
+
+  precio1= parseInt(document.getElementById("txtIdPrecioUno").value);
+  precio2= parseInt(document.getElementById("txtIdPrecioDos").value);
+  precio3= parseInt(document.getElementById("txtIdPrecioTres").value);
+
+  suma= precio1+precio2+precio3;
+
+  alert("la suma de los precio es: "+ suma);
 }
+
 function Promedio () 
 {
-	
+  let precio1, precio, precio3, promedio;
+
+  precio1= parseInt(document.getElementById("txtIdPrecioUno").value);
+  precio2= parseInt(document.getElementById("txtIdPrecioDos").value);
+  precio3= parseInt(document.getElementById("txtIdPrecioTres").value);
+
+  promedio= (precio1+precio2+precio3)/3;
+
+  alert("El promedio es: "+ promedio);
 }
+
 function PrecioFinal () 
 {
-	
+  let precio1, precio, precio3, precioTotal, iva, precioFinal;
+
+  precio1= parseInt(document.getElementById("txtIdPrecioUno").value);
+  precio2= parseInt(document.getElementById("txtIdPrecioDos").value);
+  precio3= parseInt(document.getElementById("txtIdPrecioTres").value);
+  
+  precioTotal= precio1+precio2+precio3;
+  iva= precioTotal*0.21;
+  precioFinal= precioTotal + iva;
+
+  alert("El precio final por los 3 productos mas IVA es de: "+ precioFinal);
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,43 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+ let largo, ancho, alambre, alambreTotal;
 
+ largo= parseInt(document.getElementById("txtIdLargo").value);
+ ancho= parseInt(document.getElementById("txtIdAncho").value);
+
+ alambre= (largo*2)+(ancho*2);
+ alambreTotal= alambre * 3;
+
+alert("La cantidad de alambre a comprar son: "+alambreTotal+" metros");
 }
+
 function Circulo () 
 {
-	
+ let radio, perimetro, alambreTotal;
+ radio= parseInt(document.getElementById("txtIdRadio").value);
+ 
+ //Sacando el perimetro se la circuferencia del circulo, y por lo tanto se cuanta cantidad de alambre debo comprar.
+ perimetro= (2 * 3.14)* radio;
+
+ alambreTotal= perimetro*3;
+
+ alert("La cantidad de alambre a comprar son: "+ alambreTotal+" metros");
 }
+
 function Materiales () 
 {
-	
+// cada 1 metro cuadrado son 2 de cemento y 3 de cal
+
+ let cal, cemento, largo, ancho, metrosCuadrados;
+
+ largo= parseInt(document.getElementById("txtIdLargo").value);
+ ancho= parseInt(document.getElementById("txtIdAncho").value);
+
+ metrosCuadrados= largo*ancho;
+
+ cal= metrosCuadrados*3;
+ cemento= metrosCuadrados*2;
+
+alert("Para los " + metrosCuadrados + " metros cuadrados de su propiedad debe comprar "+cemento+" bolsas de cemento y "+ cal+ " de cal");
 }
